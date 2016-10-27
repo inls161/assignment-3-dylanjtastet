98d34d0007b6d84b6c6eee067b96017d790865b0
Dylan Tastet <dylanjtastet@gmail.com>
2016-10-27 06:01:51 +0000

    Added some comments

--- dylanjtastet-convert-docs.sh
+++ dylanjtastet-convert-docs.sh
@@ -4,13 +4,13 @@
 FILENAME=$1
 
 read -p "New Filename (exclude ext): " NEWNAME
-# Convert markdown to HTML
+# Convert markdown to HTML. -s Gives the html headers so it can be deployed as a webpage
 pandoc --css=gridlines.css --smart -s -o "$NEWNAME.html" "$FILENAME"  # doesn't change anything # && pandoc --smart -V geometry:margin=1in -o "$NEWNAME-better.pdf" "$NEWNAME.html"
 # Convert markdown to DOCX
 pandoc --smart -o "$NEWNAME.docx" "$FILENAME" 
 # Convert markdown to ODT
 pandoc --smart -o "$NEWNAME.odt" "$FILENAME"
-# Convert markdown to PDF
+# Convert markdown to PDF. Specify 1in margins.
 pandoc --smart -V geometry:margin=1in -o "$NEWNAME.pdf" "$FILENAME"
 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
